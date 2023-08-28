cdbe360ca895566adc277052e805da648117234a
ojinikim <ojinikim@gmail.com>
2023-08-28 23:33:48 +0900

    💄:: 아이콘 추가

--- src/Icon/Close.jsx
+++ src/Icon/Close.jsx
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Close() {
+function Close({ closeModal }) {
+  const [hovered, setHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setHovered(false);
+  };
+
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="36"
-      height="36"
+      width="28"
+      height="28"
       viewBox="0 0 36 36"
       fill="none"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      style={{ cursor: "pointer" }}
+      onClick={closeModal} // 호출될 함수를 전달
     >
+      <rect
+        width="36"
+        height="36"
+        rx="18"
+        fill={hovered ? "#F5F6F6" : "transparent"}
+      />
       <path
         d="M10 10L26 26M10 26L26 10"
         stroke="#C9CAD0"
---
+++ src/Icon/Heart.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+function Heart() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="none"
+    >
+      <g clip-path="url(#clip0_6023_4099)">
+        <path
+          d="M8.20058 2.10052C9.89183 0.582408 12.5054 0.632807 14.1344 2.26528C15.7635 3.89774 15.8195 6.49854 14.3043 8.19483L8.19979 14.309L2.09537 8.19483C0.580283 6.49854 0.636917 3.89363 2.26527 2.26528C3.89548 0.635075 6.50443 0.580155 8.20058 2.10052Z"
+          fill="#FF7A1B"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_6023_4099">
+          <rect
+            width="15"
+            height="15"
+            fill="white"
+            transform="translate(0.600098 0.5)"
+          />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
+
+export default Heart;
